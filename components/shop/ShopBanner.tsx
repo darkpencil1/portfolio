453a1360d64c6de03a869b5985988e4fca94fa71
Ville Lähetkangas <lahetkan@protonmail.com>
2025-03-30 12:11:48 +0300

    Fix typo

--- components/shop/ShopBanner.tsx
+++ components/shop/ShopBanner.tsx
@@ -6,7 +6,7 @@ export const ShopBanner = () => {
       <div className={styles.banner__textContainer}>
         <h1 className={styles.banner__title}>Shop</h1>
         <p className={styles.banner__desc}>
-          {`Fancy an original painting to cover your wall? I have commisions open.`}
+          {`Fancy an original painting to cover your wall? I have commissions open.`}
         </p>
       </div>
     </div>
